878135c4d0a15ce0c3af28ad122af57922f75034
vergilx6x <med.work96@hotmail.com>
2024-10-16 11:36:49 +0000

    Task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create users table with id, email, name --
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255));
